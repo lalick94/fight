b747284ef04b337aa1f6dd6c850369d73e79411b
Kristina Lalic <kristina.lalic@pwc.com>
2023-03-08 18:06:01 +0100

    Rename entity

--- backend/src/model/username.sequence.ts
+++ backend/src/model/username.sequence.ts
@@ -1,7 +1,7 @@
 import {Column, Entity, PrimaryColumn, PrimaryGeneratedColumn} from "typeorm";
 
 
-@Entity()
+@Entity({ name: "username_seq" })
 export class UsernameSequence {
     @PrimaryColumn()
     public id!: string;
